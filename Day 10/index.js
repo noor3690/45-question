6e231d6363d500389e250680786474b6385c27db
noorasif17 <162204402+noorasif17@users.noreply.github.com>
2024-05-03 09:51:04 -0600

    45 ques

---
+++ Day 10/index.js
@@ -0,0 +1,43 @@
+"use strict";
+// Question 28
+let age = 30;
+if (age < 2) {
+    console.log("The person is a Baby");
+}
+else if (age < 4) {
+    console.log("The person is a Toodler");
+}
+else if (age < 13) {
+    console.log("The person is a Kid");
+}
+else if (age < 20) {
+    console.log("The person is a Teenager");
+}
+else if (age < 65) {
+    console.log("The person is an Adult");
+}
+else {
+    console.log("The person is an Elder");
+}
+// Question 29
+let favorite_fruits = ["Mango", "Apple", "Grapes"];
+if (favorite_fruits.includes('Apple')) {
+    console.log("You really like Apple!");
+}
+if (favorite_fruits.includes("Mango")) {
+    console.log("You really like Mango!");
+}
+if (favorite_fruits.includes("Grapes")) {
+    console.log("You really like Grapes!");
+}
+// Question 30
+let usernames = ["Admin", "user1", "user2", "user3"];
+usernames.forEach(username => {
+    if (username === "Admin") {
+        console.log(`Hello Admin, would you like to see a status report?`);
+    }
+    else {
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    }
+});
+console.log('THANK YOU');
--- Personal Message/app.js
+++
@@ -1,7 +0,0 @@
-//let personname : string= "Noor ul ain";
-//console.log(" hellow, " + "personname" +  "would you like to eat burger")
-//let personname : string= "Noor ul ain";
-//console.log(`hellow ${personname} would you like to eat burger`);
-var personName = "Noor ul ain";
-// E.g. hellow would you like to eat burger, Noor ul ain
-console.log("hellow ".concat(personName, " would you like to eat burger"));
